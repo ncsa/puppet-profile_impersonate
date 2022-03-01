7e7e2493f31d614bc232ed70ab76e737a663504a
David King <kingda@illinois.edu>
2022-02-28 18:26:26 -0600

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile_impersonate
+class profile_impersonate {
+  Array $impersonaters,
+  Array $imposterable,
+}
